6b7809908119cd1c81abde4a48e5d4291c6e83a8
freifeldroyi <freifeldroyi@localhost>
2010-04-17 22:28:31 +0300

    Added test suit (AKA tests_main)
    NOTE:
    now main is free to be the REAL main function of this project

--- main.c
+++ main.c
@@ -5,10 +5,7 @@
  *      Author: Freifeld Royi
  */
 #include "include/queue.h"
-#include "tests/include/test_threads.h"
-#include "tests/include/test_queue.h"
 #include "include/thread.h"
-#include "tests/include/test_app.h"
 #include <stdio.h>
 #include <assert.h>
 
@@ -17,25 +14,5 @@ int main()
 	app_main(1,"app");
 
 	return 0;
-}
-
-void queue_tests()
-{
-	run_queue_tests();
-}
-
-void priority_shced_tests()
-{
-	run_prio_scheduler_tests();
-}
-
-void threads_tests()
-{
-	run_thread_test();
-}
-
-void app_test_cases()
-{
-	run_app_tests();
 }
 
